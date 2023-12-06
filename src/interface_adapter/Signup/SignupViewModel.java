db3f3ac62cf13e4e7515b5fedd66456ac8f58c62
Ben Zheng <benz.zheng@mail.utoronto.ca>
2023-12-06 12:10:37 -0500

    changed look and feel

--- src/interface_adapter/Signup/SignupViewModel.java
+++ src/interface_adapter/Signup/SignupViewModel.java
@@ -6,14 +6,14 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class SignupViewModel extends ViewModel {
-    public static final String TITLE_LABEL = "Sign Up View";
-    public static final String USERNAME_LABEL = "Enter username";
-    public static final String PASSWORD_LABEL = "Enter password";
-    public static final String REPEAT_PASSWORD_LABEL = "Enter password again";
-    public static final String GENDER_LABEL = "Enter your gender (male or female)";
-    public static final String WEIGHT_LABEL = "Enter weight (kg)";
-    public static final String HEIGHT_LABEL = "Enter height (cm)";
-    public static final String AGE_LABEL = "Enter your age";
+    public static final String TITLE_LABEL = "Sign Up";
+    public static final String USERNAME_LABEL = "Create a username:";
+    public static final String PASSWORD_LABEL = "Create a password:";
+    public static final String REPEAT_PASSWORD_LABEL = "Re-enter password:";
+    public static final String GENDER_LABEL = "Gender (male or female):";
+    public static final String WEIGHT_LABEL = "Weight (kg):";
+    public static final String HEIGHT_LABEL = "Height (cm):";
+    public static final String AGE_LABEL = "Age:";
     public static final String DIETARY_RESTRICTIONS_LABEL = "Select your dietary restrictions";
     public static final String WEEKLY_BUDGET_LABEL = "Enter your weekly budget";
     public static final String SIGNUP_BUTTON_LABEL = "Sign up";
